ef391ec1092c5a45969b78ad6197d0b174a0659a
Amar <amar164@gmail.com>
2025-06-14 22:15:47 +0300

    project-card

--- src/app/project-card/project-card.ts
+++ src/app/project-card/project-card.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project-card',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './project-card.css'
 })
 export class ProjectCard {
+  @Input() projectNmae:string = " ";
 
 }
